a34b79bdbf042d26f43ec7d982e2b95f11520529
aboueleyes <ibrahimabouelanin@gmail.com>
2021-01-24 23:20:11 +0200

    needed packages

---
+++ scrape.py
@@ -0,0 +1,17 @@
+import argparse
+import getpass
+import json
+import os
+import re
+import time
+import urllib.request
+
+import requests
+import urllib3
+from bs4 import BeautifulSoup as bs
+from iterfzf import iterfzf
+from requests_ntlm import HttpNtlmAuth
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
