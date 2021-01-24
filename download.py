c7f80640cd091539f0ae6a1dc34e60e495169fc4
aboueleyes <ibrahimabouelanin@gmail.com>
2021-01-25 00:06:02 +0200

    code refactor

--- download.py
+++ download.py
@@ -1,18 +1,25 @@
-import os 
-import json
-import argparse 
+#!/usr/bin/env python
 
-from iterfzf import iterfzf 
+''' download
+use youtube-dl to download videos
+'''
+import argparse
+import json
+import os
+
+from iterfzf import iterfzf
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', type=argparse.FileType('r'))
 args = parser.parse_args()
+
 f = args.i
 s = f.read()
-s = s.replace('\'','\"')
+s = s.replace('\'', '\"')
 data = json.loads(s)
 video_names = list(data.values())
-to_be_downoaded = iterfzf(video_names,multi=True)
+to_be_downoaded = iterfzf(video_names, multi=True)
+
 for item in to_be_downoaded:
     index = video_names.index(item)
     link = list(data.keys())[index]
