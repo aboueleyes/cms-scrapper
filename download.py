d8d92d1609558e2d3a874b6d20ac5946500425d2
aboueleyes <ibrahimabouelanin@gmail.com>
2021-01-24 23:16:03 +0200

    choose files and use ydl to download it

--- download.py
+++ download.py
@@ -12,3 +12,8 @@ s = f.read()
 s = s.replace('\'','\"')
 data = json.loads(s)
 video_names = list(data.values())
+to_be_downoaded = iterfzf(video_names,multi=True)
+for item in to_be_downoaded:
+    index = video_names.index(item)
+    link = list(data.keys())[index]
+    os.system(f'youtube-dl  "{link}" -o "{item}.mp4"')
