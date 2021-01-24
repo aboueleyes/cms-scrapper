5caa746c15713e8dee71c194ea9aa45c1fe5b29d
aboueleyes <ibrahimabouelanin@gmail.com>
2021-01-24 23:29:43 +0200

    login func

--- scrape.py
+++ scrape.py
@@ -1,6 +1,9 @@
+"""scrape
+This script allows the user to scarpe all video links and save it to a file
+"""
+
 import argparse
 import getpass
-import json
 import os
 import re
 import time
@@ -15,15 +18,15 @@ from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-# args options 
+# args options
 praser = argparse.ArgumentParser()
 praser.add_argument('-o')
 args = praser.parse_args()
 
-# ssl Warning 
+# ssl Warning
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-# selenium options 
+# selenium options
 caps = DesiredCapabilities.CHROME
 caps['goog:loggingPrefs'] = {'performance': 'ALL'}
 options = webdriver.ChromeOptions()
@@ -31,3 +34,21 @@ options.add_argument('headless')
 options.add_argument("--window-size=1920,1080")
 driver = webdriver.Chrome(desired_capabilities=caps, options=options)
 
+
+def get_credinalities():
+    ''' login to cms website'''
+    if not os.path.isfile(".env"):
+        username = input("Enter your username : ")
+        password = getpass.getpass(prompt="Enter Your Password : ")
+        file_env = open(".credenalites", "w")
+        file_env.write(username+"\n"+password)
+        file_env.close()
+    else:
+        file_env = open("env", "r")
+        lines = file_env.readlines()
+        username = lines[0].strip()
+        password = lines[1].strip()
+        file_env.close()
+        return username, password
+
+username, password = get_credinalities()
