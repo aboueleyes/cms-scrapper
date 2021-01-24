5d4db10fd6c54a857b3c963ff6b8c72553150c86
aboueleyes <ibrahimabouelanin@gmail.com>
2021-01-24 23:15:06 +0200

    read the json file

--- download.py
+++ download.py
@@ -4,4 +4,11 @@ import argparse
 
 from iterfzf import iterfzf 
 
-
+parser = argparse.ArgumentParser()
+parser.add_argument('-i', type=argparse.FileType('r'))
+args = parser.parse_args()
+f = args.i
+s = f.read()
+s = s.replace('\'','\"')
+data = json.loads(s)
+video_names = list(data.values())
