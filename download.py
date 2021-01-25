a16e397bed0e46627580a5ca99c8701cb586f8bb
aboueleyes <ibrahimabouelanin@gmail.com>
2021-01-25 20:14:35 +0200

    remove alive progress

--- download.py
+++ download.py
@@ -6,7 +6,6 @@ use youtube-dl to download videos
 import argparse
 import json
 import os
-from alive_progress import alive_bar
 from iterfzf import iterfzf
 
 parser = argparse.ArgumentParser()
@@ -19,10 +18,8 @@ data = json.loads(s)
 video_names = list(data.values())
 to_be_downoaded = iterfzf(video_names, multi=True)
 
-with alive_bar(len(to_be_downoaded), title='Downloading videos', bar='filling') as bar:
-    for item in to_be_downoaded:
-        index = video_names.index(item)
-        link = list(data.keys())[index]
-        command = f'ffmpeg-bar -i "{link}"  -bsf:a aac_adtstoasc -vcodec copy -c copy -crf 50 "{item}.mp4"'
-        os.system(command)
-        bar()
+for item in to_be_downoaded:
+    index = video_names.index(item)
+    link = list(data.keys())[index]
+    command = f'ffmpeg-bar -i "{link}"  -bsf:a aac_adtstoasc -vcodec copy -c copy -crf 50 "{item}.mp4"'
+    os.system(command)
