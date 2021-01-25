a558c980013035773a111e45d8655da96bf5b2b1
aboueleyes <ibrahimabouelanin@gmail.com>
2021-01-25 20:01:31 +0200

    progress bar

--- download.py
+++ download.py
@@ -6,10 +6,8 @@ use youtube-dl to download videos
 import argparse
 import json
 import os
-import m3u8
+from alive_progress import alive_bar
 from iterfzf import iterfzf
-import mder
-import wget 
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', type=argparse.FileType('r'))
@@ -21,9 +19,10 @@ data = json.loads(s)
 video_names = list(data.values())
 to_be_downoaded = iterfzf(video_names, multi=True)
 
-for item in to_be_downoaded:
-    index = video_names.index(item)
-    link = list(data.keys())[index]
-    command = f'ffmpeg-bar -i "{link}"  -bsf:a aac_adtstoasc -vcodec copy -c copy -crf 50 "{item}.mp4"'
-    os.system(command)
-
+with alive_bar(len(to_be_downoaded), title='Downloading videos', bar='filling') as bar:
+    for item in to_be_downoaded:
+        index = video_names.index(item)
+        link = list(data.keys())[index]
+        command = f'ffmpeg-bar -i "{link}"  -bsf:a aac_adtstoasc -vcodec copy -c copy -crf 50 "{item}.mp4"'
+        os.system(command)
+        bar()
